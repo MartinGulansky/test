38d1a310e223155b9bb0cf3991121a11d74aa9d7
unknown <martin.gulansky@gmail.com>
2020-12-28 00:13:41 +0100

    zmeny v javascripte

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("ahoj som pripojeny k indexu, ha ha");
+class moja_trieda 
+{
+    vek = 5;
+    constructor(num)
+    {
+        this.vek = num;
+    }
+}
+
+var trieda = new moja_trieda("5");
+let moja2_trieda = new moja_trieda("100")
+console.log(trieda.vek);
+console.log(moja2_trieda.vek);
